86947d75a42cdcf821c5e35778cba0ec478ba2b5
Simon Vetter <vetter@esa.tu-darmstadt.de>
2023-08-29 13:30:15 +0200

    Added figure for tracking changes in average availability and prices over time (#2)

--- analyze.py
+++ analyze.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+import matplotlib.dates as mdates
 from dateutil.parser import isoparse as parse_iso_date
 
 
@@ -8,8 +9,6 @@ from dateutil.parser import isoparse as parse_iso_date
 filter_price_per_person = 100
 
 window_title = "Room availability analysis – JUFA Hotel Bregenz 2024"
-plot_title = "Available rooms below " + str(filter_price_per_person) + "€ by date and price per person"
-plot_subtitle = "Circle size shows number of rooms available. All prices include (only) breakfast and are adjusted for higher ticket prices on Fr/Sa."
 
 # offsets for ticket category 4 (95€/108€/121€)
 ticket_price_offset_fr = -95 + 108
@@ -72,20 +71,21 @@ def parse_csv(filepath):
 def parse_all_csv(path):
 	result = []
 	
-	for filename in os.listdir(path):
+	filenames = sorted(os.listdir(path))
+	for filename in filenames:
 		if filename.endswith('.csv'):
 			csv_lines = parse_csv(os.path.join(path, filename))
-			result.append(csv_lines[1:])
+			result.append((csv_lines[1:], filename[:-4]))
 	
 	return result
 
 def parse_first_csv(path):
-	files = sorted(os.listdir(path))
-	return parse_csv(os.path.join(path, files[0]))
+	filenames = sorted(os.listdir(path))
+	return parse_csv(os.path.join(path, filenames[0]))
 
 def parse_last_csv(path):
-	files = sorted(os.listdir(path))
-	return parse_csv(os.path.join(path, files[len(files)-1]))
+	filenames = sorted(os.listdir(path))
+	return parse_csv(os.path.join(path, filenames[len(filenames)-1]))
 
 def filter_lines(lines):
 	result = []
@@ -148,10 +148,11 @@ for lines in data_by_date_first:
 
 
 
-plot_x = []
-plot_y = []
-plot_size = []
-plot_color = []
+# prepare figure 1
+fig1_x = []
+fig1_y = []
+fig1_size = []
+fig1_color = []
 
 def scale_avail(num_available):
 	return 10 + (num_available * 10) + (num_available ** 1.5) * 30
@@ -179,33 +180,88 @@ for lines in data_by_date_current:
 		max_num_gone = max(num_gone, max_num_gone)
 		
 		if num_available > 0 or num_gone > 0:
-			plot_x.append(date.strftime("%a %d.%m."))
-			plot_y.append(price_bracket)
-			plot_size.append(scale_avail(num_available))
-			plot_color.append(num_gone)
+			fig1_x.append(date.strftime("%a %d.%m."))
+			fig1_y.append(price_bracket)
+			fig1_size.append(scale_avail(num_available))
+			fig1_color.append(num_gone)
 
 
 
-fig = plt.figure()
+# FIGURE 1: scatterplot with hotness
+fig1 = plt.figure()
+fig1_plot_title = "Available rooms below " + str(filter_price_per_person) + "€ by date and price per person"
+fig1_plot_subtitle = "Circle size shows number of rooms available. All prices include (only) breakfast and are adjusted for higher ticket prices on Fr/Sa."
 plt.get_current_fig_manager().set_window_title(window_title)
-fig.suptitle(plot_title, fontsize=14)
-plt.title(plot_subtitle, fontsize=8)
-fig.subplots_adjust(left=0.05, right=1.1, top=0.9, bottom=0.13)
-fig.set_dpi(100)
-fig.set_size_inches(12, 6)
-axes = plt.gca()
-scatter = axes.scatter(plot_x, plot_y, s=plot_size, c=plot_color, cmap='summer', vmin=0, vmax=max_num_gone, alpha=1)
-axes.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.0f €'))
+fig1.suptitle(fig1_plot_title, fontsize=14)
+plt.title(fig1_plot_subtitle, fontsize=8)
+fig1.subplots_adjust(left=0.07, right=1.1, top=0.9, bottom=0.13)
+fig1.set_size_inches(12, 6)
+# scatterplot
+fig1_axes = plt.gca()
+scatter = fig1_axes.scatter(fig1_x, fig1_y, s=fig1_size, c=fig1_color, cmap='summer', vmin=0, vmax=max_num_gone, alpha=1)
+fig1_axes.set_ylabel("Price per person (rounded to " + str(price_rounding) + "€)")
+fig1_axes.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.0f €'))
+# x-axis and legends
 plt.xticks(rotation=45, ha='right')
-[tick.set_color('blue' if tick.get_text().startswith('Thu') else 'black') for tick in axes.xaxis.get_ticklabels()]
-
+[tick.set_color('blue' if tick.get_text().startswith('Thu') else 'black') for tick in fig1_axes.xaxis.get_ticklabels()]
 # create list of sizes to show in legend
 size_legend_labels = [0, 1] + [*range(2, 2 * int(max_num_avail / 2) + 1, 2)]
 size_legend_handles = [plt.scatter([],[], s=scale_avail(size_legend_labels[i]), label=size_legend_labels[i], color='gray') for i in range(len(size_legend_labels))]
 # create size legend
 plt.legend(handles=size_legend_handles, loc='lower right', labelspacing=1.8, borderpad=1.2)
-
 # create color legend
-fig.colorbar(scatter, label="Already booked or price changed", location='right', pad=0.025)
+fig1.colorbar(scatter, label="Already booked or price changed", location='right', pad=0.025)
+
+
+
+# prepare figure 2
+fig2_x = []
+fig2_y1 = []
+fig2_y2 = []
+
+all_data = parse_all_csv('collected_results')
+for data_one_scrape_date, scrape_date in all_data:
+	days = set()
+	num_avail = 0
+	num_different_rooms = 0
+	avg_price = 0
+	for line in data_one_scrape_date:
+		if line['size'] >= 4 and line['meals'] == "Übernachtung - Frühstück":
+			days.add(line['start_date'])
+			num_avail += line['num_available']
+			num_different_rooms += 1
+			avg_price += line['price'] / line['size']
+	if num_avail > 0:
+		fig2_x.append(parse_iso_date(scrape_date))
+		fig2_y1.append(num_avail / len(days))
+		fig2_y2.append(avg_price / num_different_rooms)
+
+# FIGURE 2: line plots for changes in availability and price
+fig2 = plt.figure()
+fig2_plot_title = "Available rooms (for 4 or more people) and their prices over time"
+fig2_plot_subtitle = "Prices are for rooms which include (only) breakfast."
+plt.get_current_fig_manager().set_window_title(window_title)
+fig2.suptitle(fig2_plot_title, fontsize=14)
+plt.title(fig2_plot_subtitle, fontsize=8)
+fig2.subplots_adjust(left=0.06, right=0.94, top=0.9, bottom=0.07)
+fig2.set_size_inches(12, 6)
+# plot 1 (left y-axis): number of available rooms
+fig2_ax1 = plt.gca()
+fig2_plot1 = fig2_ax1.plot(fig2_x, fig2_y1, color='blue', marker='o', label='Number of available rooms on each day')
+fig2_ax1.set_ylabel('Average of available rooms per day')
+fig2_ax1.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+# plot 2 (right y-axis): average price per person
+fig2_ax2 = fig2_ax1.twinx()
+fig2_plot2 = fig2_ax2.plot(fig2_x, fig2_y2, color='red', marker='o', label='Average price per person')
+fig2_ax2.set_ylabel('Average price per person')
+fig2_ax2.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.0f €'))
+fig2_ax2.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+# legend and x-axis
+plt.legend(handles=[fig2_plot1[0], fig2_plot2[0]], loc='lower center')
+fig2_ax1.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+fig2_ax1.xaxis.set_major_formatter(mdates.DateFormatter('%B %Y'))
+plt.xticks(rotation=45, ha='right')
+
+
 
 plt.show()

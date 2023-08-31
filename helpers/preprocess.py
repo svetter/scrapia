d147fcbc4148faeedf63e3828ff81c9690556ba0
Simon Vetter <vetter@esa.tu-darmstadt.de>
2023-08-31 10:22:16 +0200

    Added GPL3 license

--- helpers/preprocess.py
+++ helpers/preprocess.py
@@ -1,3 +1,18 @@
+# Copyright 2023 Simon Vetter
+#
+# This file is part of Scrapia.
+#
+# Scrapia is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Scrapia is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Scrapia.
+# If not, see <https://www.gnu.org/licenses/>.
+
 import collections
 
 from constants import STAY_DATES
